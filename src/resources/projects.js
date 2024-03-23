2bbb0a042553bd56f38758da40c6bc647d7e629e
Ankit Majee <ankitmajee.05@gmail.com>
2024-03-24 01:53:49 +0530

    Added Projects Section

---
+++ src/resources/projects.js
@@ -0,0 +1,26 @@
+export const projects = [
+    {
+        technologies: [],
+        id: 1,
+        title: 'React',
+        image: 'react.png',
+        description: 'A JavaScript library for building user interfaces.',
+        githubUrl: 'https://github.com/facebook/react'
+    },
+    {
+        technologies: [],
+        id: 2,
+        title: 'Tailwind CSS',
+        image: 'tailwindcss.png',
+        description: 'A utility-first CSS framework for rapidly building custom designs.',
+        githubUrl: 'https://github.com/tailwindlabs/tailwindcss'
+    },
+    {
+        technologies: [],
+        id: 3,
+        title: 'Node.js',
+        image: 'nodejs.png',
+        description: 'A JavaScript runtime built on Chrome\'s V8 JavaScript engine.',
+        githubUrl: ' '
+    }
+];
