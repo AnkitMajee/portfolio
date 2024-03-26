2f84fdb8e58e6408bdd7062b416096a49ef9bf21
Ankit Majee <ankitmajee.05@gmail.com>
2024-03-26 17:03:56 +0530

    Updating Render Deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+import path from 'path';
 
 const dbConfig = require('./config/dbConfig');
 
@@ -8,7 +9,15 @@ const portfolioroute = require('./routes/portfolioRoute')
 app.use(express.json());
 app.use("/api/portfolio", portfolioroute);
 
+const __dirname = path.resolve();
+
 const port =process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
